09488c367fa84573f1abaa0bb8322f033d3b3c6a
ad0ll <ad0ll@users.noreply.github.com>
2022-12-07 10:41:13 -0800

    First pass

---
+++ .changeset/strange-goats-sin.md
@@ -0,0 +1,11 @@
+---
+'@chainlink/dns-record-check-adapter': patch
+'@chainlink/nftx-adapter': patch
+'@chainlink/reference-transform-adapter': patch
+'@chainlink/ea-bootstrap': patch
+'@chainlink/example-composite-adapter': patch
+'@chainlink/binance-dex-adapter': patch
+'@chainlink/blockstream-adapter': patch
+---
+
+Fake changeset
--- packages/scripts/src/gha/lib.ts
+++ packages/scripts/src/gha/lib.ts
@@ -1,4 +1,5 @@
 import { DockerLabels, generateFileJSON } from '../docker-build/lib'
+import fs from 'fs'
 
 interface JobMatrix {
   adapter: {
@@ -12,16 +13,36 @@ interface JobMatrix {
  * docker images
  */
 export async function getJobMatrix(): Promise<JobMatrix> {
+  // yarn generate:gha:matrix to only include changed adapters
+  // yarn generate:gha:matrix full to include all adapters
   const branch = process.env.BRANCH || ''
   const prefix = process.env.IMAGE_PREFIX || ''
   const useLatest = !!process.env.LATEST
   const dockerfile = await generateFileJSON({ prefix, branch, useLatest }, { context: '.' })
-  const adapter = Object.entries(dockerfile.services).map(([k, v]) => {
-    return {
-      name: k,
-      type: v.build.labels[DockerLabels.EA_TYPE],
-    }
-  })
+
+  const adapterNames = new Set<string>()
+  const expression = new RegExp(/'@chainlink\/([a-zA-Z-]*-adapter)'/, 'g')
+
+  fs.readdirSync('./.changeset')
+    .filter((file) => file.endsWith('.md'))
+    .map((file) => {
+      const content = fs.readFileSync(`./.changeset/${file}`)
+      Array.from(content.toString().matchAll(expression)).forEach((m) => {
+        adapterNames.add(m[1])
+      })
+    })
+
+  const adapter = Object.entries(dockerfile.services)
+    .filter(([k]) => {
+      // Do not filter if we are building all adapters
+      return process.argv[2] === 'full' || adapterNames.has(k)
+    })
+    .map(([k, v]) => {
+      return {
+        name: k,
+        type: v.build.labels[DockerLabels.EA_TYPE],
+      }
+    })
 
   return { adapter }
 }
