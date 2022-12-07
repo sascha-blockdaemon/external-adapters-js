8c67cd57fe301782ade3d793e0d8d5abc4259059
ad0ll <ad0ll@users.noreply.github.com>
2022-12-07 14:59:12 -0800

    Change readme to only check changed EAs

--- .github/get-changed-adapters-array.sh
+++ .github/get-changed-adapters-array.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This is a utility script to get a list of changed adapters. You can use it in a helper script with the following:
 # source .github/get-changed-adapters-array.sh
@@ -9,7 +9,6 @@
 #     done
 # # As space separated string:
 #     SPACE_SEPARATED=$(getChangedAdapterArray)
-# # As json output
 
 
 if [[ -z $UPSTREAM_BRANCH ]]; then
@@ -21,25 +20,39 @@ REGEX="@chainlink/(.*-adapter)"
 # Any echoed lines will appear in the output array.
 
 function getChangedAdapterArray(){
-  changedFiles=$( git diff --name-status HEAD "origin/$UPSTREAM_BRANCH" |
-  grep ".changeset" |
-  grep -v "README.md" | # Inverse match
-  cut -f2 )
+  changedFiles=()
+  for line in $(git diff --name-only "origin/$UPSTREAM_BRANCH"...HEAD); do
+    echo "line: $line"
+    if [[ $line =~ ^packages/sources/([a-zA-Z-]*)/.*$ ]]; then
+      # TODO sed was giving me a hard time with \sources\|composites. Need to fix later.
+      adapterName=$(echo "$line" | sed  -e 's/packages\/sources\/\(.*\)\/.*/\1/g' )
 
-  adapterSet=() # Would ideally want to use an associative array, but MacOS doesn't have bash 4+ installed and we'd have to install zsh in a run to use it
-  for fileInThisBranch in $changedFiles; do
-  while read -r line; do # See command after "done" for input
+      if [[ -n $ADAPTER_SUFFIX ]]; then
+        adapterName="$adapterName-$ADAPTER_SUFFIX"
+      fi
 
-      # Grep has extracted the string from the changeset (i.e. cut spaces, quotes, unrelated text)
-      # We can now replace the @chainlink/ prefix for the adapter name
-      # Ex: @chainlink/evm-balance-adapter -> evm-balance-adapter
-      adapterName=$(echo "$line" | sed  -e 's/.*@chainlink\/\(.*-adapter\).*/\1/g' )
-      # Check if we've already encountered this adapter
       if [[ ! "${adapterSet[*]}" =~ $adapterName ]]; then
         adapterSet+=("$adapterName")
       fi
-    done < <(grep -oE "$REGEX" "$fileInThisBranch")
+    fi
   done
+  echo "${adapterSet[@]}"
+##  whi ( git diff --name-status HEAD "origin/$UPSTREAM_BRANCH" |
+##  grep ".changeset" |
+##  grep -v "README.md" |
+##  cut -f2 )
+#echo $changedFiles
+#git diff --name-status HEAD "origin/$UPSTREAM_BRANCH"
+#  adapterSet=()
+#  for fileInThisBranch in $changedFiles; do
+#    ls "branchFile: $fileInThisBranch"
+#    while read -r line; do # See command after "done" for input
+#        adapterName=$(echo "$line" | sed  -e 's/.*@chainlink\/\(.*-adapter\).*/\1/g' )
+#        if [[ ! "${adapterSet[*]}" =~ $adapterName ]]; then
+#          adapterSet+=("$adapterName")
+#        fi
+#      done < <(grep -oE "$REGEX" "$fileInThisBranch")
+#  done
 
   echo "${adapterSet[@]}"
 }
