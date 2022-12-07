db6715f4f5a5832c1cf1e380389b735019c8c742
ad0ll <ad0ll@users.noreply.github.com>
2022-12-07 15:22:18 -0800

    Cleanup

--- .github/get-changed-adapters-array.sh
+++ .github/get-changed-adapters-array.sh
@@ -18,14 +18,12 @@ REGEX="@chainlink/(.*-adapter)"
 
 # DO NOT USE echo IN THIS SCRIPT EXCEPT TO DUMP THE RESULT
 # Any echoed lines will appear in the output array.
-
 function getChangedAdapterArray(){
-  changedFiles=()
+  adapterSet=()
   for line in $(git diff --name-only "origin/$UPSTREAM_BRANCH"...HEAD); do
     if [[ $line =~ ^packages/sources/([a-zA-Z-]*)/.*$ ]]; then
       # TODO sed was giving me a hard time with \sources\|composites. Need to fix later.
       adapterName=$(echo "$line" | sed  -e 's/packages\/sources\/\(.*\)\/.*/\1/g' )
-
       if [[ -n $ADAPTER_SUFFIX ]]; then
         adapterName="$adapterName-$ADAPTER_SUFFIX"
       fi
@@ -35,23 +33,6 @@ function getChangedAdapterArray(){
       fi
     fi
   done
-  echo "${adapterSet[@]}"
-##  whi ( git diff --name-status HEAD "origin/$UPSTREAM_BRANCH" |
-##  grep ".changeset" |
-##  grep -v "README.md" |
-##  cut -f2 )
-#echo $changedFiles
-#git diff --name-status HEAD "origin/$UPSTREAM_BRANCH"
-#  adapterSet=()
-#  for fileInThisBranch in $changedFiles; do
-#    ls "branchFile: $fileInThisBranch"
-#    while read -r line; do # See command after "done" for input
-#        adapterName=$(echo "$line" | sed  -e 's/.*@chainlink\/\(.*-adapter\).*/\1/g' )
-#        if [[ ! "${adapterSet[*]}" =~ $adapterName ]]; then
-#          adapterSet+=("$adapterName")
-#        fi
-#      done < <(grep -oE "$REGEX" "$fileInThisBranch")
-#  done
 
   echo "${adapterSet[@]}"
 }
